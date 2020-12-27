491bb4e3627d4e063cf54a793c1625aa34a068fc
Eden Tyler-Moss <eden@etylermoss.com>
2020-12-27 01:58:43 +0000

    Removed unncessary IIFE.

--- src/preload.ts
+++ src/preload.ts
@@ -2,33 +2,31 @@
  * Credit : https://stackoverflow.com/questions/3354239
 */
 window.addEventListener('DOMContentLoaded', () => {
-	(() => {
-		let activityTimer: number = null;
-		let cursorVisible: boolean = true;
-	
-		const disappearCursor = (): void =>
-		{
-			activityTimer = null;
-			document.body.style.cursor = 'none';
-			cursorVisible = false;
-		};
+	let activityTimer: number = null;
+	let cursorVisible: boolean = true;
 
-		const setActivity = (): void => {
-			if (activityTimer)
-			{
-				window.clearTimeout(activityTimer);
-			}
-	
-			if (!cursorVisible)
-			{
-				document.body.style.cursor = 'default';
-				cursorVisible = true;
-			}
-	
-			activityTimer = window.setTimeout(disappearCursor, 3000);
-		};
-	
-		document.onmousemove = setActivity;
-		document.onkeydown = setActivity;
-	})();
+	const disappearCursor = (): void =>
+	{
+		activityTimer = null;
+		document.body.style.cursor = 'none';
+		cursorVisible = false;
+	};
+
+	const setActivity = (): void => {
+		if (activityTimer)
+		{
+			window.clearTimeout(activityTimer);
+		}
+
+		if (!cursorVisible)
+		{
+			document.body.style.cursor = 'default';
+			cursorVisible = true;
+		}
+
+		activityTimer = window.setTimeout(disappearCursor, 3000);
+	};
+
+	document.onmousemove = setActivity;
+	document.onkeydown = setActivity;
 });
